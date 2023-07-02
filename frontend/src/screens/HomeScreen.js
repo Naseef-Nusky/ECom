e39d39d7d797ea2c2956fe06e50558088f8e83f4
Naseef Nusky <117664828+Naseef-Nusky@users.noreply.github.com>
2023-07-02 19:54:59 +0530

    first commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import { useState,useReducer,useEffect } from "react";
 import { Link } from "react-router-dom";
 //import data from "../data";
-import axios from 'axios'
+import axios from 'axios';
+
 
 const reducer=(state,action)=>{
   switch(action.type){
     case'FETCH_REQUEST':
       return{...state,loading:true};
     case'FETCH_SUCCESS':
-      return{...state,product: action.payload, loading:false};
+      return{...state,products: action.payload, loading:false};
     case 'FETCH_FAIL':
       return{...state, loading: false, error: action.payload};
     default:
@@ -17,31 +18,31 @@ const reducer=(state,action)=>{
 };
 
 function HomeScreen  () {
-  // const[{loading,error, products},dispatch]=useReducer(reducer,{
-  //   products:[],
-  //   loading:true,
-  //   error:'',
-  // });
+  const[{loading,error, products},dispatch]=useReducer(reducer,{
+    products:[],
+    loading:true,
+    error:'',
+  });
 
-  const[products,setProducts]=useState([]);
+ // const[products,setProducts]=useState([]);
   useEffect(()=>{
-    // const fetchData=async() =>{
-    //   dispatch({type:'FETCH_REQUEST'});
-    //   try{
-    //     const result=await axios.get('/api/products');
-    //     dispatch({type:'FETCH_SUCCESS',payload:result.data});
-    //   }catch(err){
-    //     dispatch({type:'FETCH_FAIL',payload: err.massage});
-    //   }
+    const fetchData=async() =>{
+      dispatch({type:'FETCH_REQUEST'});
+      try{
+        const result=await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCESS',payload:result.data});
+      }catch(err){
+        dispatch({type:'FETCH_FAIL',payload: err.massage});
+      }
 
-      const fetchData=async() =>{
+      // const fetchData=async() =>{
         
-          const result=await axios.get('/api/products');
-          setProducts(result.data);
+      //     const result=await axios.get('/api/products');
+      //     setProducts(result.data);
         
         
      
-     setProducts(result.data)
+   //  setProducts(result.data)
     };
     fetchData();
   },[]);
