55308505dee09bedf7a6459c0538c9f356850339
Naseef Nusky <117664828+Naseef-Nusky@users.noreply.github.com>
2023-08-10 12:27:38 +0530

    initial commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -53,17 +53,17 @@ function ProductScreen () {
     const {cart}=state;
 
     const addToCartHandler = async() => {
-      // const existItem=cart.cartItems.find((x)=> x._id===product._id)
-      // const quantity = existItem? existItem.quantity +1 :1;
-      // //const {data} = await axios.get(`/api/products/${product._id}`);
-      // if(product.countInStock  < quantity){
-      //   window.alert('Sorry.Product is out of stock')
-      //   return;
-      // } 
+      const existItem=cart.cartItems.find((x)=> x._id===product._id)
+      const quantity = existItem? existItem.quantity +1 :1;
+      const {data} = await axios.get(`/api/products/${product._id}`);
+      if(product.countInStock  < quantity){
+        window.alert('Sorry.Product is out of stock')
+        return;
+      } 
 
       ctxDispatch({ 
         type:'CART_ADD_ITEM',
-      payload:{...product,quantity:1},
+      payload:{...product,quantity},
     });
 
     }; 
