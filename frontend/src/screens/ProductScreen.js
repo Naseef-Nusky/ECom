94ba84c5e4b5e8267d0db8a58136526d90929664
Naseef Nusky <117664828+Naseef-Nusky@users.noreply.github.com>
2023-08-12 14:08:34 +0530

    initial commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -57,7 +57,7 @@ function ProductScreen () {
       const existItem=cart.cartItems.find((x)=> x._id===product._id)
       const quantity = existItem? existItem.quantity +1 :1;
       const {data} = await axios.get(`/api/products/${product._id}`);
-      if(product.countInStock  < quantity){
+      if(data.countInStock  < quantity){
         window.alert('Sorry.Product is out of stock')
         return;
       } 
